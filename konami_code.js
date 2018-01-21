9979e7a7d7748eec2580690bbae2e5b0f4107f49
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-21 00:13:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
   let index = 0;
 function init() {
-  document.body.addEventListener(`keydown`,onKeyDownHandler(`keydown`),false)}
-
-    /*function (e) {
+  document.body.addEventListener(`keydown`,function (e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
@@ -19,20 +17,4 @@ function init() {
 
   }
 )
-}
-*/
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
-
-  if (key === code[index]) {
-    index++;
-
-    if (index === code.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
 }
